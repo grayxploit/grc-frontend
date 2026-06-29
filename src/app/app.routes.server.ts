7bfe35c5bf03abaf08bf193abd539ffb98461b28
Rajib Mondal <rajib.3t@gmail.com>
2026-06-29 14:47:05 +0530

    framework category

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,32 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'dashboard',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'controls',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'control-create',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'control-import',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'frameworks',
+    renderMode: RenderMode.Client
+  },
+
+  {
+    path: 'framework-categories',
+    renderMode: RenderMode.Client
+  },
+
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
